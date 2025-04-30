3a1ea43a5df123bc3eaa604f2fabf6e1439cbd7e
baehaky <baehaky1504@gmail.com>
2025-05-01 03:07:05 +0700

    modified:   src/lib/api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    Origin: window.location.origin,
+    Origin: 'https://www.hellopet.site',
   },
 })
 
